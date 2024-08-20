6be69f4b3ebbd611d006d8b123bb32287384f8bf
Evgeny Mangasaryan <mangasaryan.ep@gmail.com>
2024-08-20 11:46:07 +0400

    feat: Add note to the random sequence function

--- internal/random/random.go
+++ internal/random/random.go
@@ -15,6 +15,10 @@ var letters = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
 
 // Generates a random sequence of letters of the alphabet
 // of a given length.
+//
+// Note: this is not a cryptographically safe source of
+// random sequences, and can be spoofed or predicted. Don't
+// use it to generate passwords!
 func RandSeq(n int) string {
 	b := make([]rune, n)
 	for i := range b {
